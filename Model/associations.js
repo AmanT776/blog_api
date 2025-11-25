4b7a42cd61ca073b8341fcfbdb7368964edce3fa
AmanT776 <amantes776@gmail.com>
2025-11-26 00:20:42 +0300

    feat:-created all associations

--- Model/associations.js
+++ Model/associations.js
@@ -5,7 +5,7 @@ const Comment = require("./Comment");
 const Images = require("./Images");
 const Post = require("./Post");
 
-//one to many relation with role and user
+//one to one relation with role and user
 
 Role.hasOne(User,{
     foreignKey: "role_id"
@@ -16,18 +16,47 @@ User.belongsTo(Role,{
 
 //one to many relation with user and post
 
-User.hasOne(Post,{
+User.hasMany(Post,{
     foreignKey: "user_id"
-})
+});
 
 Post.belongsTo(User,{
     foreignKey: "user_id"
-})
+});
 
+//one to many relation with post and category
+Category.hasMany(Post,{
+    foreignKey: "category_id"
+});
 
+Post.belongsTo(Category,{
+    foreignKey: "category_id"
+});
 
+//one to many relation with post and comment 
+Post.hasMany(Comment,{
+    foreignKey: "post_id",
+});
+
+Comment.belongsTo(Post,{
+    foreignKey: "post_id"
+});
+
+//one to many relation with post and images
+Post.hasMany(Images,{
+    foreignKey: "post_id"
+});
+
+Images.belongsTo(Post,{
+    foreignKey: "post_id"
+});
 
 module.exports = {
-    User,Role
+    User,
+    Role,
+    Post,
+    Comment,
+    Images,
+    Category
 }
 
