ae7fb3389e65643858f2831427693435fa5271c1
AmanT776 <amantes776@gmail.com>
2025-11-26 20:14:26 +0300

    fix:-fixed models

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const db = require('./Config/db');
-const {User} = require('./Model/associations');
+const db = require("./Config/db");
 
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
-
-(async()=>{
-    try{
-        await db.sync({force: true});
-        console.log("all database synced")
-        app.listen(PORT,()=>{
-          console.log(`the server is running on http://localhost:${PORT}`);
-        })
-    }catch(error){
-        console.log(error);
-    }
+(async () => {
+  try {
+    await db.sync({ force: true });
+    console.log("all database synced");
+    app.listen(PORT, () => {
+      console.log(`the server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 })();
-
