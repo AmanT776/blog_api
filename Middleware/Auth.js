8d78e9c51675b21ab461bf5b26e6519a00cb769a
AmanT776 <amantes776@gmail.com>
2025-11-29 04:41:56 +0300

    fix:-refactored auth middleware logic

--- Middleware/Auth.js
+++ Middleware/Auth.js
@@ -1,26 +1,25 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
+const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+
 export const authenticate = (req, res, next) => {
   const token = req.headers["Authorization"].split(" ")[1];
 
   if (!token) {
     return res.status(401).json({
       success: false,
-      message: "No token Provided",
+      message: "Unautorized access",
     });
   }
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    if (decoded) {
-      next();
-    } else {
-      return res.status(401).json({
-        success: false,
-        message: "invalid access token",
-      });
-    }
+    const decoded = jwt.verify(token, ACCESS_TOKEN_SECRET);
+    req.user = decoded;
+    next();
   } catch (error) {
-    console.log(error);
+    return res.status(403).res({
+      success: false,
+      messages: "Invalid or expired token",
+    });
   }
 };
