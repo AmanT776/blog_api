7e0a1ea3f8f77d814d7612b5e7366d72dc3a7dbe
AmanT776 <amantes776@gmail.com>
2025-12-05 19:42:05 +0300

    feat:-configured cloudinary

---
+++ Config/cloudinary.js
@@ -0,0 +1,10 @@
+const cloudinary = require("cloudinary").v2;
+require("dotenv").config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: CLOUDINARY_KEY,
+  api_secret: CLOUDINARY_SECRET,
+});
+
+module.exports = cloudinary;
